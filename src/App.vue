6171652775b09c161a6ff36162e010758458eb1b
teo <teo@nudgecoach.com>
2023-07-27 11:45:24 -0400

    Init (Vue3)

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import AcademicFitReport from "./components/AcademicFitReport.vue";
+import AthleteData from "./assets/data.json";
+
+const athlete_info = ref(AthleteData.data[0])
+</script>
+
+<template>
+  <div class="container">
+    <AcademicFitReport :athlete="athlete_info" />
+  </div>
+</template>
+
---
+++ src/components/AcademicFitReport.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+
+interface Athlete {
+  name: String,
+  sport: String,
+  grad_year: Number,
+  gpa: Number,
+  major: String,
+  club: { name: String },
+  high_school: { name: String }
+}
+
+defineProps<{
+  athlete: Athlete
+}>();
+
+</script>
+
+<template>
+  <div class="info">
+    <h2>{{ athlete.name }}</h2>
+    <ul>
+      <li>
+        <label>Sport:</label>
+        {{ athlete.sport }}
+      </li>
+      <li>
+        <label>Class:</label>
+        {{ athlete.grad_year }}
+      </li>
+      <li>
+        <label>Club:</label>
+        {{ athlete.club.name }}
+      </li>
+      <li>
+        <label>High School:</label>
+        {{ athlete.high_school.name }}
+      </li>
+      <li>
+        <label>GPA:</label>
+        {{ athlete.gpa }}
+      </li>
+      <li>
+        <label>Desired Major:</label>
+        {{ athlete.major }}
+      </li>
+    </ul>
+  </div>
+</template>
+
